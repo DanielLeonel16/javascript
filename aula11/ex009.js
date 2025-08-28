c49726d085f8f392d27b18fb8c0e68d9dbcd2772
DanielLeonel16 <142305879+DanielLeonel16@users.noreply.github.com>
2025-08-28 17:44:18 -0300

    mais aulas

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil'){ // TBM PODE USAR != //
+    console.log('Vc é Brasileiro!')
+} else {
+    console.log('Vc é Estrangeiro')
+}
