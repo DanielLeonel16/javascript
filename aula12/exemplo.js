05701a67532f9c0938130c59d7747d985aebc279
DanielLeonel16 <142305879+DanielLeonel16@users.noreply.github.com>
2025-08-30 19:17:44 -0300

    mais aulas

---
+++ aula12/exemplo.js
@@ -0,0 +1,26 @@
+/*
+
+var numero = 7
+
+if (numero === 2 || numero % 2 === 0) {
+    console.log('Seu número é PAR!')
+} else {
+    console.log('Seu número é IMPAR!')
+}
+
+*/
+
+var numero = 22  // ou outro número que você quiser testar
+
+if (numero % 3 === 0 && numero % 5 === 0) {
+    console.log('É múltiplo de 3 e 5')
+} else if (numero % 3 === 0) {
+    console.log('É múltiplo de 3')
+} else if (numero % 5 === 0) {
+    console.log('É múltiplo de 5')
+} else {
+    console.log('Não é múltiplo de 3 nem de 5')
+}
+
+
+
