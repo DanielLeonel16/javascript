76f4a6386659333e95beb55ec954c8aec3f05f21
DanielLeonel16 <142305879+DanielLeonel16@users.noreply.github.com>
2025-09-14 15:36:31 -0300

    fim do curso

--- aula16/funcao02.js
+++ aula16/funcao02.js
@@ -2,4 +2,5 @@ function soma(n1=0, n2=0) {
     return n1 + n2
 }
 
-console.log(soma(7))
+console.log(soma(7, 5))
+// NESTE CASO SE TIVER SOMENTE UM NUMERO VAI USAR 0
--- aula16/funcao05.js
+++ aula16/funcao05.js
@@ -9,4 +9,12 @@ function fatorial(n) {
 
 console.log(fatorial(5))
 
-// 5! = 5 x 4 x 3 x 2 x 1 = 120
+/*
+
+5! = 5 x 4 x 3 x 2 x 1 = 120
+5! = 5 x 4!
+
+n! = n x (n-1)!
+1! = 1
+
+*/
